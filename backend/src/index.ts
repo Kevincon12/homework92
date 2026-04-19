0116dbb60e6b7ef2c905adc0fd251ea4f6dbb672
Strelnikov Ilia <ily_strelnikov@mail.ru>
2026-04-19 14:18:13 +0600

    инициализация сервера: express + mongodb

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/chat");
+
+app.get("/", (req, res) => {
+    res.send({ message: "API working" });
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
